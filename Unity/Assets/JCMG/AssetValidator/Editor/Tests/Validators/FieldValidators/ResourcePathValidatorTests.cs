6145812cd0506813a0ff6e2ab16611a1991987a5
Jeff Campbell <jeff@zapdot.com>
2019-04-13 16:57:02 +0200

    Updated license header on code files to reflect MIT
    * Updated license header on code files to reflect MIT. It had always been MIT, but did not say so at the top of the file before.

--- Unity/Assets/JCMG/AssetValidator/Editor/Tests/Validators/FieldValidators/ResourcePathValidatorTests.cs
+++ Unity/Assets/JCMG/AssetValidator/Editor/Tests/Validators/FieldValidators/ResourcePathValidatorTests.cs
@@ -1,6 +1,7 @@
 ﻿/*
-AssetValidator
-Copyright (c) 2018 Jeff Campbell
+MIT License
+
+Copyright (c) 2019 Jeff Campbell
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
 using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
